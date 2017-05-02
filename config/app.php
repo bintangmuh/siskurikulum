bc117fc53d31301780c363c706560a645ecf0fc6
Bintang Muhammad <bintangmhd@gmail.com>
2017-05-02 15:08:47 +0700

    :boom:  adding admin LTE

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sistem Kurikulum PSBO'),
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        // Admin LTE
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
 
     ],
 
